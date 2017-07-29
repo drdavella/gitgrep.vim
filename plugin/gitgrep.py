a5a9457a522a6600dbcaa146fb7fd866e53d8a11
Dan D'Avella <drdavella@gmail.com>
2017-07-29 12:49:05 -0400

    handle keyboard interrupt

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -4,10 +4,13 @@ import random
 
 def gitgrep():
     while(True):
-        vim.command('let inputchar = nr2char(getchar())')
-        char = vim.eval('inputchar')
-        # Handle escape
-        if ord(char) == 27:
+        try:
+            vim.command('let inputchar = nr2char(getchar())')
+            char = vim.eval('inputchar')
+            # Handle escape
+            if ord(char) == 27:
+                break
+            vim.current.buffer[0] = char
+            vim.command('redraw!')
+        except KeyboardInterrupt:
             break
-        vim.current.buffer[0] = char
-        vim.command('redraw!')
