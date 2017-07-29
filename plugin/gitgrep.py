d70133d57a3941794b195365ec44975acd46da13
Dan D'Avella <drdavella@gmail.com>
2017-07-29 11:32:39 -0400

    Another commit

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -1,4 +1,9 @@
 import vim
+import time
+import random
 
 def gitgrep():
-    print(vim.current.buffer)
+    for x in range(10):
+        vim.current.buffer[:] = [str(random.random()) for x in range(20)]
+        time.sleep(2)
+
