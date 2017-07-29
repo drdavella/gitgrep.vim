46b28b6e528c6961271a45d117eaa9a721609a6a
Dan D'Avella <drdavella@gmail.com>
2017-07-29 12:51:48 -0400

    Handle weird input

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -2,13 +2,17 @@ import vim
 import time
 import random
 
+ESCAPE_CHAR = 27
+
 def gitgrep():
     while(True):
         try:
             vim.command('let inputchar = nr2char(getchar())')
             char = vim.eval('inputchar')
             # Handle escape
-            if ord(char) == 27:
+            if char == None or char == '':
+                continue
+            if ord(char) == ESCAPE_CHAR:
                 break
             vim.current.buffer[0] = char
             vim.command('redraw!')
