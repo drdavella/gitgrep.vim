2d6c6d859749b2da08ab8eabdf54a65c529ee8c4
Dan D'Avella <drdavella@gmail.com>
2017-07-29 12:32:19 -0400

    iasi

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -3,8 +3,7 @@ import time
 import random
 
 def gitgrep():
-    for x in range(10):
-        vim.current.buffer[:] = [str(random.random()) for x in range(20)]
-        vim.command('redraw!')
-        time.sleep(2)
+    while(True):
+        char = vim.command('nr2char(getchar())')
+        print(char)
 
