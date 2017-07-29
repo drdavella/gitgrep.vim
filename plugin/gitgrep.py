e0bb6c02bc050b6bdfdd86b29a689dc02bc0ce76
Dan D'Avella <drdavella@gmail.com>
2017-07-29 12:46:11 -0400

    handle escape

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -6,5 +6,6 @@ def gitgrep():
     while(True):
         vim.command('let inputchar = nr2char(getchar())')
         char = vim.eval('inputchar')
-        print(char)
-
+        # Handle escape
+        if ord(char) == 27:
+            break
