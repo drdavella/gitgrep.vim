cd6f157f4f73f320918233620c2abf9c4d158157
Dan D'Avella <drdavella@gmail.com>
2017-07-29 15:47:11 -0400

    Adding some framework

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -4,17 +4,52 @@ import random
 
 ESCAPE_CHAR = 27
 
-def gitgrep():
+def _run_and_return(command):
+    varname = 'tempvar'
+    vim.command('let {} = {}'.format(varname, command))
+    return vim.eval(varname)
+
+def _run_gitgrep():
+    # Just populate with dummy data for now
+    return ['hello', 'world', 'file', 'whatever', 'blurg']
+
+def _save_screen_state():
+    current_line = _run_and_return('line(".")')
+    current_col = _run_and_return('col(".")')
+    top_line = _run_and_return('w0')
+    return (current_line, current_col, top_line)
+
+def _restore_screen_state(screen_state):
+    current_line, current_col, top_line = screen_state
+    vim.command('call cursor({}, 1)'.format(top_line))
+    vim.command('normal! zt')
+    vim.command('call cursor({}, {})'.format(current_line, current_col))
+
+def _get_user_input():
+    return _run_and_return('nr2char(getchar())')
+
+def _display_and_handle(results):
+    vim.current.buffer[:] = results
+    vim.command('redraw!')
+
     while(True):
         try:
-            vim.command('let inputchar = nr2char(getchar())')
-            char = vim.eval('inputchar')
+            char = _get_user_input()
             # Handle escape
             if char == None or char == '':
                 continue
             if ord(char) == ESCAPE_CHAR:
                 break
-            vim.current.buffer[0] = char
+            vim.current.buffer.append(char)
             vim.command('redraw!')
         except KeyboardInterrupt:
             break
+
+def gitgrep():
+    results = run_gitgrep()
+    if not results:
+        return
+
+    screen_state = save_screen_state()
+    _display_and_handle(results)
+    restore_screen_state(screen_state)
