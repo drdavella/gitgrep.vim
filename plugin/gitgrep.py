6627b2bd75f3fc93d208d0882a0e7119e184dbaa
Dan D'Avella <drdavella@gmail.com>
2017-07-29 12:37:44 -0400

    getchar

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -4,6 +4,7 @@ import random
 
 def gitgrep():
     while(True):
-        char = vim.command('call nr2char(getchar())')
-        print(char)
+        char = vim.command('let gitgrep:char nr2char(getchar())')
+        print('vars:',vim.vars.get('gitgrep:char'))
+        print('vvars:',vim.vvars.get('gitgrep:char'))
 
