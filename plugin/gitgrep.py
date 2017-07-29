2fec8d6d877655b8d4625bac93e6455e403a1c52
Dan D'Avella <drdavella@gmail.com>
2017-07-29 12:14:10 -0400

    Another update

--- plugin/gitgrep.py
+++ plugin/gitgrep.py
@@ -5,5 +5,6 @@ import random
 def gitgrep():
     for x in range(10):
         vim.current.buffer[:] = [str(random.random()) for x in range(20)]
+        vim.command('edit')
         time.sleep(2)
 
